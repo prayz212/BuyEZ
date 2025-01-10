fe7f64c1a1fc42d5c8b5766ba54258cab42529e3
Vi Diec <49938194+prayz212@users.noreply.github.com>
2025-01-10 14:54:12 +0700

    fix: minor adjustment

--- src/Shared/Infrastructure/EnumArrayToStringConverter.cs
+++ src/Shared/Infrastructure/EnumArrayToStringConverter.cs
@@ -5,7 +5,7 @@ namespace Shared.Infrastructure;
 public class EnumArrayToStringConverter<TEnum> : ValueConverter<TEnum[], string> where TEnum : struct, Enum
 {
     public EnumArrayToStringConverter() : base(
-        v => string.Join(',', v.Select(x => x.ToString())), // Enum Array to String
-        v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Enum.Parse<TEnum>(x)).ToArray()) // String to Enum Array
+        v => string.Join(", ", v.Select(x => x.ToString())), // Enum Array to String
+        v => v.Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => Enum.Parse<TEnum>(x)).ToArray()) // String to Enum Array
     {}
 }
