a46b2f7189803dbfac88fd880ceb1339f72ef286
Vi Diec <49938194+prayz212@users.noreply.github.com>
2025-03-26 14:25:54 +0700

    feat: add revoke token api

--- src/Identity/Application/Shared/RestAPIs/IIdentityServerApi.cs
+++ src/Identity/Application/Shared/RestAPIs/IIdentityServerApi.cs
@@ -8,4 +8,8 @@ public interface IIdentityServerApi
     [Headers("Content-Type: application/x-www-form-urlencoded")]
     [Post("/connect/token")]
     Task<ExpandoObject> PostGetTokenAsync([Body(BodySerializationMethod.UrlEncoded)] FormUrlEncodedContent encodedContent);
+
+    [Headers("Content-Type: application/x-www-form-urlencoded")]
+    [Post("/connect/revocation")]
+    Task PostRevokeTokenAsync([Body(BodySerializationMethod.UrlEncoded)] FormUrlEncodedContent encodedContent);
 }
