6e571af78caf6723a073cc3940afe2a2fa87fda8
Vi Diec <49938194+prayz212@users.noreply.github.com>
2025-04-18 10:00:11 +0700

    fix: correct OrderHistoryResponse properties

--- src/Order.API/Application/Shared/Dtos/OrderDetailResponse.cs
+++ src/Order.API/Application/Shared/Dtos/OrderDetailResponse.cs
@@ -4,6 +4,6 @@ namespace OrderAPI.Application.Shared.Dtos;
 
 public record OrderItemResponse(string Id, string Name, double Price, int Quantity, double Amount);
 
-public record OrderHistoryResponse(string Id, OrderStatus Status, string Reason, DateTimeOffset createAt);
+public record OrderHistoryResponse(string Id, OrderStatus Status, string Reason, DateTimeOffset createdAt);
 
 public record OrderDetailResponse(string Id, OrderCustomerInfo CustomerInfo, OrderStatus Status, double TotalAmount, List<OrderItemResponse> Items, List<OrderHistoryResponse> Histories, DateTimeOffset createdAt);
